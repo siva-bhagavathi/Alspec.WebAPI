7495ed0698423bcb0a36d63907b1cfcb75bdf6a8
Siva Nellai <siva.nellai@yahoo.com>
2024-11-19 10:10:53 +1100

    Add project files.

---
+++ Alspec.WebAPI/Models/Job.cs
@@ -0,0 +1,10 @@
+﻿namespace Alspec.WebAPI.Models
+{
+    public class Job
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<SubItem> SubItems { get; set; }
+    }
+}
---
+++ Alspec.WebAPI/Models/SubItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Alspec.WebAPI.Models
+{
+    public class SubItem
+    {
+        public string ItemId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; } 
+        public string Status { get; set; }
+    }
+}
